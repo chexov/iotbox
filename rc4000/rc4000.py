a3f6c7f093831b625a399190b17a711c8f168fe1
Anton P. Linevich <anton@linevich.com>
2021-07-25 19:27:27 -0700

    simclient

--- rc4000/rc4000.py
+++ rc4000/rc4000.py
@@ -20,116 +20,148 @@ def addressbook():
     return ("+380",)
 
 
-def gt_auth(pin: int):
-    gt_sendcmd(b"GT+AUTH=%s" % pin)
+class RCMode(Enum):
+    NORMAL: int = 0
+    ACCESS: int = 1
+    DOOR: int = 2
+    UNKNOWN: int = 3
 
 
-def gt_info():
-    gt_sendcmd(b"GT+INFO")
+class DeviceState:
+    rcmode: RCMode
+    pin: int
 
 
-def gt_sendcmd(cmd: bytes):
-    log.debug("sending '%s'", cmd)
-    ser.write(cmd + b"\r\n")
+class SIMSerialClient:
+    def __init__(self, port: str, baudrate: int, authpin: int = 1111):
+        self._ser = Serial(
+            port,
+            baudrate=baudrate,
+            bytesize=EIGHTBITS,
+            parity=PARITY_NONE,
+            stopbits=STOPBITS_ONE,
+            timeout=1,
+        )
+        # self._ser.flushInput()
+        self.state = DeviceState()
+        self.state.pin = authpin
 
+    def cmd_callback(self, line: bytes):
+        print("line=%s" % line)
+        if line == "GT: READY":
+            self.gt_info()
+        elif line == b'+GETDEV: "RC-4000"':
+            self.on_getdev()
+        elif line.startswith(b"+REB: "):
+            self.on_reboot()
+        elif line.startswith(b"+GTINCALL: "):
+            phone = line.replace(b"+GTINCALL: ", b"")
+            self.on_incall(phone)
+        elif line.startswith(b"+GT: CALL DIS"):
+            self.on_callend()
+        elif line == b"+GTLOAD: 1":
+            self.on_loadon()
+        elif line == b"+GTLOAD: 0":
+            self.on_loadoff()
+        elif line.startswith(b"+GTWRPN: "):
+            phone = line.replace(b"+GTINCALL: ", b"")
+            log.debug("new number: %s", phone)
+            self.gt_info()
+        elif line.startswith(b"+GETMD: "):
+            mode = line.replace(b"+GETMD: ", b"")
+            self.on_getmd(mode)
 
-def gt_getmd():
-    gt_sendcmd(b"GT+GETMD")
+        elif line.startswith(b"+AUTH: "):
+            result = line.replace(b"+AUTH: ", b"")
+            self.on_auth(result)
+        elif line.startswith(b"+GTSMS: "):
+            sms = line.replace(b"+GTINCALL: ", b"")
+            self.on_sms(sms)
+        else:
+            log.error("unknown %s", line)
 
+    def gt_auth(self, pin: int):
+        self.gt_sendcmd(b"GT+AUTH=%s" % pin)
 
-def gt_getpwd():
-    gt_sendcmd(b"GT+GETPWD")
+    def gt_info(self):
+        self.gt_sendcmd(b"GT+INFO")
 
+    def gt_sendcmd(self, cmd: bytes):
+        log.debug("sending '%s'", cmd)
+        self._ser.write(cmd + b"\r\n")
 
-def gt_load1():
-    gt_sendcmd(b"GT+GTLOAD=1")
+    def gt_getmd(self):
+        self.gt_sendcmd(b"GT+GETMD")
 
+    def gt_getpwd(self):
+        self.gt_sendcmd(b"GT+GETPWD")
 
-def cmd_callback(line: bytes):
-    print("line=%s" % line)
-    if line == "GT: READY":
-        gt_info()
-    elif line == b'+GETDEV: "RC-4000"':
-        log.debug("found rc-4000")
-    elif line.startswith(b"+REB: "):
-        log.debug("rebooting")
-    elif line.startswith(b"+GTINCALL: "):
-        phone = line.replace(b"+GTINCALL: ", b"")
-        log.debug("in call from %s", phone)
+    def gt_load1(self):
+        self.gt_sendcmd(b"GT+GTLOAD=1")
 
-        if phone in addressbook():
-            gt_load1()
+    def on_sms(self, sms):
+        log.debug("in sms: %s", sms)
 
-    elif line.startswith(b"+GT: CALL DIS"):
-        log.debug("in call ended")
-    elif line == b"+GTLOAD: 1":
-        log.debug("relay ON")
-    elif line == b"+GTLOAD: 0":
-        log.debug("relay OFF")
-    elif line.startswith(b"+GTWRPN: "):
-        phone = line.replace(b"+GTINCALL: ", b"")
-        log.debug("new number: %s", phone)
-        gt_info()
-    elif line.startswith(b"+GETMD: "):
-        mode = line.replace(b"+GETMD: ", b"")
+    def on_auth(self, result):
+        if result == b"1":
+            log.debug("auth ok")
+            self.gt_info()
+            self.gt_getmd()
+            # gt_getpwd()
+        else:
+            log.debug("auth error")
+
+    def on_getmd(self, mode):
         log.debug("mode is %s", mode)
-
-        class RCMode(Enum):
-            NORMAL: int = 0
-            ACCESS: int = 1
-            DOOR: int = 2
-            UNKNOWN: int = 3
-
         try:
             log.debug(RCMode(value=int(mode)))
         except ValueError:
             log.debug("mode is UNKNOWN")
 
-    elif line.startswith(b"+AUTH: "):
-        result = line.replace(b"+AUTH: ", b"")
-        if result == b"1":
-            log.debug("auth ok")
-            gt_info()
-            gt_getmd()
-            # gt_getpwd()
-        else:
-            log.debug("auth error")
-    elif line.startswith(b"+GTSMS: "):
-        sms = line.replace(b"+GTINCALL: ", b"")
-        log.debug("in sms: %s", sms)
-    else:
-        log.error("unknown %s", line)
+    def on_loadoff(self):
+        log.debug("relay OFF")
+
+    def on_loadon(self):
+        log.debug("relay ON")
+
+    def on_callend(self):
+        log.debug("in call ended")
+
+    def on_incall(self, phone):
+        log.debug("in call from %s", phone)
+        if phone in addressbook():
+            self.gt_load1()
+
+    def on_reboot(self):
+        log.debug("rebooting")
+
+    def on_getdev(self):
+        log.debug("found rc-4000")
+
+    def loop(self):
+        line: bytes = b""
+        while True:
+            b = self._ser.read(size=1)
+            # print("b=%s" % b)
+            if not b:
+                # empty read on timeout
+                continue
+
+            if b == b"\r":
+                line = line.strip()  # remove "\n"?
+                if not line:
+                    continue
+
+                self.cmd_callback(line)
+                line = b""
+            else:
+                line = line + b
 
 
 def main(port: str = "/dev/cu.usbserial-401210", baudrate: int = 115200):
-    # Prepare serial connection.
-    ser = Serial(
-        port,
-        baudrate=baudrate,
-        bytesize=EIGHTBITS,
-        parity=PARITY_NONE,
-        stopbits=STOPBITS_ONE,
-        timeout=1,
-    )
-
-    # ser.flushInput()
-
-    gt_auth(8052)
-
-    line: bytes = b""
-    while True:
-        b = ser.read(size=1)
-        # print("b=%s" % b)
-        if not b:
-            # empty read
-            continue
-
-        if b == b"\r":
-            line = line.strip()  # remove "\n"?
-            cmd_callback(line)
-            line = b""
-        else:
-            line = line + b
+    simcli = SIMSerialClient(port=port, baudrate=baudrate, authpin=8042)
+    simcli.gt_auth(8052)
+    simcli.loop()
 
 
 if __name__ == "__main__":
